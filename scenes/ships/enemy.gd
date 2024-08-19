03374f520e2a6a2aa715a2c3883c39da181fa8e4
Finnn <varvells@gmail.com>
2024-08-19 21:35:19 +1000

    minimap enemy jitter fix

--- scenes/ships/enemy.gd
+++ scenes/ships/enemy.gd
@@ -2,6 +2,11 @@ class_name Enemy extends Ship
 
 @onready var minimap_stamp: Sprite2D = $Head/MinimapStamp
 
+func _ready() -> void:
+	super._ready()
+	minimap_stamp.reparent(self.get_parent())
+	died.connect(remove_stamp)
+	
 
 func _move() -> void:
 	move(level.DIRECTIONS.pick_random())
@@ -16,3 +21,6 @@ func _move() -> void:
 		stamp_pos.x = clamp(stamp_pos.x, player_pos.x - minimap_size, player_pos.x + minimap_size)
 		stamp_pos.y = clamp(stamp_pos.y, player_pos.y - minimap_size, player_pos.y + minimap_size)
 		minimap_stamp.global_position = stamp_pos
+
+func remove_stamp():
+	minimap_stamp.queue_free()
